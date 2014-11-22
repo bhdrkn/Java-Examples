3a890061ca844d727512d71fdf756dd76f4af6e8
Bahadır AKIN <bhdrkn@gmail.com>
2014-11-22 23:55:56 +0200

    DAO layer is removed, import.sql is added.

--- simple-smpp-routing/src/main/java/com/bahadirakin/App.java
+++ simple-smpp-routing/src/main/java/com/bahadirakin/App.java
@@ -1,7 +1,5 @@
 package com.bahadirakin;
 
-import com.bahadirakin.dao.SmDao;
-import com.bahadirakin.entities.ShortMessage;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,18 +12,7 @@ public class App {
         applContext.registerShutdownHook();
         System.out.println("Application is started");
 
-        SmDao dao = applContext.getBean(SmDao.class);
-
-        final ShortMessage shortMessage = new ShortMessage();
-        shortMessage.setMessage("Hello, World!");
-        shortMessage.setDestinationAddress("05323332211");
-        shortMessage.setDestinationNpi((byte) 0);
-        shortMessage.setDestinationTon((byte) 0);
-
-        dao.persist(shortMessage);
-
         System.in.read();
-
 
     }
 }
