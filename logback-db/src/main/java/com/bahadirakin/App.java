0f319fd57dc446ab4ce5fcb2b884f88c15f143c2
Bahadır AKIN <bhdrkn@gmail.com>
2015-09-03 09:25:40 +0300

    Logback-DB appender example

---
+++ logback-db/src/main/java/com/bahadirakin/App.java
@@ -0,0 +1,16 @@
+package com.bahadirakin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+    }
+}
